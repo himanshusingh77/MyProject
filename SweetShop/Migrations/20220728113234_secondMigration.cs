0549b12e509efbc610eb0384e922996288b05fe9
Himanshu Singh <himanshu.singh6@globallogic.com>
2022-07-29 15:21:33 +0530

    MyProject

---
+++ SweetShop/Migrations/20220728113234_secondMigration.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SweetShop.Migrations
+{
+    public partial class secondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_product_City_cityID",
+                table: "product");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_City",
+                table: "City");
+
+            migrationBuilder.RenameTable(
+                name: "City",
+                newName: "cities");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_cities",
+                table: "cities",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_product_cities_cityID",
+                table: "product",
+                column: "cityID",
+                principalTable: "cities",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_product_cities_cityID",
+                table: "product");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_cities",
+                table: "cities");
+
+            migrationBuilder.RenameTable(
+                name: "cities",
+                newName: "City");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_City",
+                table: "City",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_product_City_cityID",
+                table: "product",
+                column: "cityID",
+                principalTable: "City",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
